96c42fedc49840de48832e8affed39f84c507e1b
Fauzi Kliman <fauzimerfoo@gmail.com>
2019-11-18 19:18:24 -0800

    Add descriptions to each card
    - Change "[Source]" to "[GitHub]" for improved clarity

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,7 @@
             Hello, I am a soon to graduate (March 2020) from Oregon State University with a Bachelor of Science in Computer Science.
           </p>
           <p>
-            I like to write software in C/C++, Python 3, and JavaScript for people to use and enjoy!
+            I like to write software in C++, Python 3, JavaScript, C#, and C for people to use and enjoy!
           </p>
           <p>
           </p>
@@ -32,8 +32,9 @@
                   <h1 class="title is-5">
                     Finite State Machine
                   </h1>
+                  <p>Create finite state machine diagrams that you can download and share with others!</p>
                   <a href="https://merfoo.github.io/fsm/">[Live]</a>
-                  <a href="https://github.com/Merfoo/fsm">[Source]</a>
+                  <a href="https://github.com/Merfoo/fsm">[GitHub]</a>
                 </template>
               </BulmaCard>
 
@@ -47,8 +48,9 @@
                   <h1 class="title is-5">
                     Asteroids
                   </h1>
+                  <p>Traverse through procedurally generated asteroids coming at you from all directions with only movement and lazer attacks to assist you!</p>
                   <a href="https://merfoo.github.io/Asteroidz/">[Live]</a>
-                  <a href="https://github.com/Merfoo/Asteroidz">[Source]</a>
+                  <a href="https://github.com/Merfoo/Asteroidz">[GitHub]</a>
                 </template>
               </BulmaCard>
             </div>
@@ -63,8 +65,9 @@
                   <h1 class="title is-5">
                     Breakout
                   </h1>
+                  <p>The classic Breakout game with the ability to create custom levels and download a picture of the level!</p>
                   <a href="https://merfoo.github.io/Breakout/">[Live]</a>
-                  <a href="https://github.com/Merfoo/Breakout">[Source]</a>
+                  <a href="https://github.com/Merfoo/Breakout">[GitHub]</a>
                 </template>
               </BulmaCard>
 
@@ -78,8 +81,9 @@
                   <h1 class="title is-5">
                     Snake
                   </h1>
+                  <p>A crazy colorful snake game featuring multiple game modes that you can play with friends!</p>
                   <a href="https://merfoo.github.io/Epileptic-Snake-Game/">[Live]</a>
-                  <a href="https://github.com/Merfoo/Epileptic-Snake-Game">[Source]</a>
+                  <a href="https://github.com/Merfoo/Epileptic-Snake-Game">[GitHub]</a>
                 </template>
               </BulmaCard>
             </div>
